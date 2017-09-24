67927b320dcaf4ba7695a186318421f2be962b26
Nathan R Smith <nrsmith012@gmail.com>
2017-09-23 18:14:40 -0700

    time app first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Time Display App';
+
+  westCoastTime = 1;
+  timeZones: String[] = ["East Coast", "West Coast"];
+
+  chkTime = () => {
+    var t = new Date();
+    if (!this.westCoastTime){
+      t.setHours(t.getHours()-3);
+    }
+    return (t+'').split(' GMT')[0];
+  }
+
+  toggleTime = () => {
+    this.westCoastTime = (this.westCoastTime + 1) % 2;
+  }
+
+  showTZ = () => this.timeZones[this.westCoastTime];
+
+  showOtherTZ = () => this.timeZones[(this.westCoastTime+1)%2];
+  
+
 }
+
